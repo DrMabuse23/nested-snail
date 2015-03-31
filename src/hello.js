f7413d3c0e1a00e0ef518456a5d6134d9a21e459
DrMabuse23 <drmabuse@posteo.de>
2015-03-31 23:08:53 +0200

    add first step boilerplate

---
+++ src/hello.js
@@ -0,0 +1,19 @@
+import {Component, Template} from 'angular2/angular2';
+import {If} from 'angular2/angular2';
+
+@Component({
+  selector: 'hello'
+})
+@Template({
+  inline: `<span *if="name">Hello, {{name}}!</span>`,
+  directives: [If]
+})
+export class Hello {
+  name:string;
+  constructor() {
+    this.name = 'World';
+    setTimeout(() => {
+      this.name = 'Snail'
+    }, 2000);
+  }
+}
